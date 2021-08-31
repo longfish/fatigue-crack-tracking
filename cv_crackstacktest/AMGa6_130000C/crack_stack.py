85884c28af694990fd21bb8ee42a7ac612cdd226
Changyu Meng (Student) <cmeng12@sundevils.asu.edu>
2021-08-30 18:47:09 -0700

    compute the geometric information of the crack

--- cv_crackstacktest/AMGa6_130000C/crack_stack.py
+++ cv_crackstacktest/AMGa6_130000C/crack_stack.py
@@ -1,6 +1,16 @@
+'''
+Compute the crack geometry from a stack of crack images
+
+Usage:
+  put the script into the crack images parent path of a loading sample
+
+'''
+
 import numpy as np
 import cv2 as cv
 import os
+
+PIXEL = 0.0061  # 1 pixel == 0.0061 mm
 
 
 def crack_detection(img, h, th_g):
@@ -59,50 +69,99 @@ def find_max_contour(img):
     return max_cont
 
 
-def crack_extraction(img):
+def crack_hull_extraction(img):
     '''
     Extract crack from the noised image
-    Output: the convex hull image of the crack
+    Output: the convex hull of the crack
     '''
     kernel = np.ones((2, 2), np.uint8)
     crack_final = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
     max_cont = find_max_contour(crack_final)
     crack_hull = cv.convexHull(max_cont)
-    blank = np.zeros((736, 736), np.uint8)
-    cv.drawContours(blank, [crack_hull], 0, 250, -1)
 
-    return blank
+    return crack_hull
 
 
-# find the directories of the crack, and list the crack images
-cwd = os.getcwd()
-folders = [dir for dir in os.listdir(cwd) if os.path.isdir(dir)]
+def crack_geo_calc(c, cnt):
+    '''
+    Calculate the geometric information of the contour
+    Input: c = (cx, cy, r); cnt is the contour of crack
+    Output: area, depth, side_length of the contour (pixel length)
+    '''
+    area = cv.contourArea(cnt)
 
-for f in folders:
-    # loop the folders
-    dir = os.path.join(cwd, f)
-    imgs = os.listdir(dir)
-    crack_final = np.zeros((736, 736), np.uint8)
-    for img in imgs:
-        # loop the images in the crack folder
-        img_dir = os.path.join(dir, img)
-        img_array = cv.imread(img_dir, 0)
-        crack = crack_detection(img_array, 12, 100)
-        # cv.imshow(img, crack)
+    min_ang = np.arctan2(cnt[0][0][0]-c[0], cnt[0][0][1]-c[1])
+    max_ang = min_ang
+    min_dis = 1000
+    for p in cnt:
+        ang = np.arctan2(p[0][0]-c[0], p[0][1]-c[1])
+        if (ang < 0):
+            ang += 2*np.pi
 
-        cx, cy, r = cross_section_contour(
-            img_array, 14)  # get the contour circle
-        # create a circle mask
-        cir_mask = np.zeros(img_array.shape[:2], np.uint8)
-        cv.circle(cir_mask, (cx, cy), r, 255, thickness=-1)
+        print(ang)
+        # modify the [min,max] range of the angle
+        if(ang < min_ang):
+            min_ang = ang
+        elif(ang > max_ang):
+            max_ang = ang
 
-        # apply bitwise operation to obtain the crack object
-        crack = cv.bitwise_not(crack, mask=cir_mask)
-        crack_final = cv.add(crack_final, crack)  # add all the images together
+        # find the minimum distance between contour points and center
+        dis = np.sqrt((cnt[0][0][0]-c[0])*(cnt[0][0][0]-c[0]) +
+                      (cnt[0][0][1]-c[1])*(cnt[0][0][1]-c[1]))
+        if(dis < min_dis):
+            min_dis = dis
 
-    crack_final = crack_extraction(crack_final)
-    cv.imshow('Final crack object', crack_final)
+    print(c[2], min_dis, max_ang, min_ang)
+    return area, (c[2]-min_dis), c[2]*(max_ang-min_ang)
 
 
-cv.waitKey()
-cv.destroyAllWindows()
+def main():
+    cwd = os.getcwd()  # find the directories of the crack, and list the crack images
+    folders = [dir for dir in os.listdir(cwd) if (
+        os.path.isdir(dir) and str.isalnum(dir))]
+
+    for f in folders:
+        # loop the folders
+        dir = os.path.join(cwd, f)
+        imgs = os.listdir(dir)
+        crack_final = np.zeros((736, 736), np.uint8)
+        cx_stack = []
+        cy_stack = []
+        r_stack = []
+        for img in imgs:
+            # loop the images in the crack folder
+            img_dir = os.path.join(dir, img)
+            img_array = cv.imread(img_dir, 0)
+            crack = crack_detection(img_array, 12, 100)
+            # cv.imshow(img, crack)
+
+            cx, cy, r = cross_section_contour(
+                img_array, 14)  # get the contour circle
+            # create a circle mask
+            cir_mask = np.zeros(img_array.shape[:2], np.uint8)
+            cv.circle(cir_mask, (cx, cy), r, 255, thickness=-1)
+            cx_stack.append(cx)
+            cy_stack.append(cy)
+            r_stack.append(r)
+
+            # apply bitwise operation to obtain the crack object
+            crack = cv.bitwise_not(crack, mask=cir_mask)
+            # add all the images together
+            crack_final = cv.add(crack_final, crack)
+
+        crack_hull = crack_hull_extraction(crack_final)
+        (area, depth, side_length) = crack_geo_calc(
+            (np.mean(cx_stack), np.mean(cy_stack), np.mean(r_stack)), crack_hull)
+        print(area*PIXEL*PIXEL, depth*PIXEL, side_length*PIXEL)
+
+        # draw the crack on a blank plate
+        blank = np.zeros((736, 736), np.uint8)
+        cv.drawContours(blank, [crack_hull], 0, 250, -1)
+        cv.imshow('Final crack object', blank)
+    cv.waitKey()
+
+
+if __name__ == '__main__':
+    print(__doc__)
+    main()
+    cv.destroyAllWindows()
