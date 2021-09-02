a5d7dd404dc91bc8a3267d507f73a79d229a8719
Longfish-Meng <loongfish@foxmail.com>
2021-09-02 00:40:07 -0700

    finished the loop folder routine

--- cv_crackstacktest/crackmeasure.py
+++ cv_crackstacktest/crackmeasure.py
@@ -18,7 +18,7 @@ def image_filter(img, h, th_g):
     '''
 
     equ = cv.equalizeHist(img)  # apply global hist equalization
-    gaus = cv.GaussianBlur(equ, (7, 7), 0)  # denoising using gaussian filter
+    gaus = cv.GaussianBlur(equ, (9, 9), 0)  # denoising using gaussian filter
     # denoising using nonlocal means algorithm
     dst1 = cv.fastNlMeansDenoising(gaus, None, h, 7, 21)
 
@@ -71,8 +71,10 @@ def cracks_extraction(img, num=1):
     Output: 
         a list of cracks (max to min area)
     '''
-    kernel = np.ones((2, 2), np.uint8)
+    kernel = np.ones((3, 3), np.uint8)
+    # break thin bifurcations
     img = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
+    # fill small holes
     img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
     cnts = contour_sort_area(img)
 
@@ -115,9 +117,9 @@ def crack_geo_calc(c, cnt):
     # find the central angle of the crack hull
     max_ang = 0
     for p in hull:
-        vec1 = [p[0][0]-c[0], p[0][1]-c[1]]
+        vec1 = np.array([p[0][0]-c[0], p[0][1]-c[1]])
         for q in hull:
-            vec2 = [q[0][0]-c[0], q[0][1]-c[1]]
+            vec2 = np.array([q[0][0]-c[0], q[0][1]-c[1]])
             ang = find_angle(vec1, vec2)
             if(ang > max_ang):
                 max_ang = ang
@@ -128,62 +130,68 @@ def crack_geo_calc(c, cnt):
 def find_angle(vec1, vec2):
     '''
     Find the angle between vectors vec1 and vec2
-    Input: vec1, vec2 = [x1, x2]
+    Input: vec1, vec2 are np.array
     '''
-    unit_vec1 = vec1/np.linalg.norm(vec1)
-    unit_vec2 = vec2/np.linalg.norm(vec2)
-    dot_product = np.dot(unit_vec1, unit_vec2)
-    return np.arccos(dot_product)
+    cos = vec1.dot(vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2))
+    return np.arccos(np.clip(cos, -1.0, 1.0))
 
 
 def main():
+    crack_info = open('crack_info.txt', 'w')
     cwd = os.getcwd()  # find the directories of the crack, and list the crack images
-    folders = [dir for dir in os.listdir(cwd) if (
-        os.path.isdir(dir) and str.isalnum(dir))]
+    stepfolders = [dir for dir in os.listdir(cwd) if (
+        os.path.isdir(dir))]
 
-    for f in folders:
-        # loop the folders
-        dir = os.path.join(cwd, f)
-        imgs = os.listdir(dir)
-        img_test = cv.imread(os.path.join(dir, imgs[0]), 0)  # image size
-        crack_final = np.zeros(img_test.shape[:2], np.uint8)
-        cx_stack = []
-        cy_stack = []
-        r_stack = []
-        for img in imgs:
-            # loop the images in the crack folder
-            img_dir = os.path.join(dir, img)
-            img_array = cv.imread(img_dir, 0)
-            crack = image_filter(img_array, 12, 100)
-            # cv.imshow(img, crack)
+    crack_info.write("Crack: Area, Depth, Side length\n")
+    # loop the loading step folders
+    for stepf in stepfolders:
+        step_dir = os.path.join(cwd, stepf)
+        crackfolders = [dir for dir in os.listdir(step_dir)]
 
-            cx, cy, r = cross_section_contour(
-                img_array, 14)  # get the contour circle
-            # create a circle mask
-            cir_mask = np.zeros(img_array.shape[:2], np.uint8)
-            cv.circle(cir_mask, (cx, cy), r, 255, thickness=-1)
-            cx_stack.append(cx)
-            cy_stack.append(cy)
-            r_stack.append(r)
+        # loop the cracks folders
+        for crackf in crackfolders:
+            crack_dir = os.path.join(step_dir, crackf)
+            imgs = os.listdir(crack_dir)
+            img_ref = cv.imread(os.path.join(
+                crack_dir, imgs[0]), 0)  # image size
+            crack_final = np.zeros(img_ref.shape[:2], np.uint8)
+            cx_stack = []
+            cy_stack = []
+            r_stack = []
 
-            # apply bitwise operation to obtain the crack object
-            crack = cv.bitwise_not(crack, mask=cir_mask)
-            # add all the images together
-            crack_final = cv.add(crack_final, crack)
+            # loop the crack images in the current folder
+            for img in imgs:
+                img_array = cv.imread(os.path.join(crack_dir, img), 0)
+                crack = image_filter(img_array, 12, 98)
+                # cv.imshow(img, crack)
 
-        # cv.imshow("Added crack ", crack_final)
-        crack_num = 1
-        crack_hub = cracks_extraction(crack_final, crack_num)
-        for c in crack_hub:
-            (area, depth, side_length) = crack_geo_calc(
-                (np.mean(cx_stack), np.mean(cy_stack), np.mean(r_stack)), c)
-            print("Crack-"+f+": area =", area*PIXEL*PIXEL, ", depth =",
-                  depth*PIXEL, ", side length =", side_length*PIXEL)
+                cx, cy, r = cross_section_contour(
+                    img_array, 14)  # get the contour circle
+                # create a circle mask, store the circle size
+                cir_mask = np.zeros(img_array.shape[:2], np.uint8)
+                cv.circle(cir_mask, (cx, cy), r, 255, thickness=-1)
+                cx_stack.append(cx)
+                cy_stack.append(cy)
+                r_stack.append(r)
 
-            # draw the crack on a blank plate
-            blank = np.zeros(img_test.shape[:2], np.uint8)
-            cv.drawContours(blank, [c], 0, 250, -1)
-            cv.imshow("Crack-"+f, blank)
+                # apply bitwise operation to obtain the crack object
+                crack = cv.bitwise_not(crack, mask=cir_mask)
+                # add all the images together
+                crack_final = cv.add(crack_final, crack)
+
+            # cv.imshow("Added crack ", crack_final)
+            crack_num = 1
+            crack_hub = cracks_extraction(crack_final, crack_num)
+            for i, c in enumerate(crack_hub):
+                (area, depth, side_length) = crack_geo_calc(
+                    (np.mean(cx_stack), np.mean(cy_stack), np.mean(r_stack)), c)
+                crack_info.write(stepf+"-"+crackf+"-"+str(i)+": "+str(area*PIXEL*PIXEL)+", " +
+                                 str(depth*PIXEL)+", "+str(side_length*PIXEL)+"\n")
+
+                # draw the crack on a blank plate
+                blank = np.zeros(img_ref.shape[:2], np.uint8)
+                cv.drawContours(blank, [c], 0, 250, -1)
+                cv.imwrite(stepf+"-"+crackf+"-"+str(i)+".png", blank)
     cv.waitKey()
 
 
