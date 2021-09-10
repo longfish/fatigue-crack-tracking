d6fdfa397b9475985425ce1ca7db233bf421f0e1
Longfish-Meng <loongfish@foxmail.com>
2021-09-10 00:16:44 -0700

    added the crack before crack measure

---
+++ cv_crackstacktest/AMGa6_160000C/crackmeasure2.py
@@ -0,0 +1,40 @@
+'''
+Compute the crack geometry from a stack of crack images
+'''
+
+import heapq as pq
+import numpy as np
+import cv2 as cv
+import os
+
+PIXEL = 0.0061  # 1 pixel == 0.0061 mm
+
+
+def main():
+    cwd = os.getcwd()
+    crackfolders = [dir for dir in os.listdir(cwd) if (
+        os.path.isdir(dir))]
+
+    # loop the crack folders
+    for crackf in crackfolders:
+        crack_dir = os.path.join(cwd, crackf)
+        imgs = os.listdir(crack_dir)  # all crack images
+        crack_final = cv.imread(os.path.join(
+            crack_dir, imgs[0]), 0)
+
+        # loop the crack images in the current folder and add together
+        for img in imgs:
+            img_array = cv.imread(os.path.join(crack_dir, img), 0)
+
+            # apply bitwise operation to obtain the crack object
+            # crack_final = cv.bitwise_xor(img_array, crack_final)
+            crack_final = cv.min(img_array, crack_final)
+
+        cv.imshow("Crack final", crack_final)
+    cv.waitKey()
+
+
+if __name__ == '__main__':
+    print(__doc__)
+    main()
+    cv.destroyAllWindows()
