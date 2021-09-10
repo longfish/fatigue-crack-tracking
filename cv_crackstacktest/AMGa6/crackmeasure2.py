62597a247394f81e56d6a0253fe6a2f6ca62f580
Changyu Meng (Student) <cmeng12@sundevils.asu.edu>
2021-09-10 10:56:12 -0700

    tested the add images method

--- cv_crackstacktest/AMGa6/crackmeasure2.py
+++ cv_crackstacktest/AMGa6/crackmeasure2.py
@@ -4,6 +4,7 @@ Compute the crack geometry from a stack of crack images
 
 import heapq as pq
 import numpy as np
+import crackmeasure as cmeas
 import cv2 as cv
 import os
 
@@ -13,24 +14,29 @@ PIXEL = 0.0061  # 1 pixel == 0.0061 mm
 def main():
     cwd = os.getcwd()
     crackfolders = [dir for dir in os.listdir(cwd) if (
-        os.path.isdir(dir))]
+        os.path.isdir(dir) and dir.isalnum())]
 
+    print(crackfolders)
     # loop the crack folders
     for crackf in crackfolders:
         crack_dir = os.path.join(cwd, crackf)
         imgs = os.listdir(crack_dir)  # all crack images
-        crack_final = cv.imread(os.path.join(
-            crack_dir, imgs[0]), 0)
+
+        img_ref = cv.imread(os.path.join(
+            crack_dir, imgs[0]), 0)  # image size
+        crack_final = np.full(img_ref.shape[:2], 255, dtype=np.uint8)
 
         # loop the crack images in the current folder and add together
         for img in imgs:
             img_array = cv.imread(os.path.join(crack_dir, img), 0)
+            img_array = cmeas.image_filter(img_array, 12, 100)
 
             # apply bitwise operation to obtain the crack object
-            # crack_final = cv.bitwise_xor(img_array, crack_final)
-            crack_final = cv.min(img_array, crack_final)
+            # crack_final = cv.bitwise_and(img_array, crack_final)
+            crack_final = cv.bitwise_xor(img_array, crack_final)
+            # crack_final = cv.min(img_array, crack_final)
 
-        cv.imshow("Crack final", crack_final)
+        cv.imshow("Crack final"+crackf, crack_final)
     cv.waitKey()
 
 
