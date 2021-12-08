f209f548878b000104bc88b57fc6e13cf39628e8
longfish <loongfish@foxmail.com>
2021-12-08 10:27:15 -0700

    renewed the combined cracks folder

---
+++ cv_crackstacktest/AMGa6/crack_extract.py
@@ -0,0 +1,265 @@
+'''
+Extract the crack geometry from a stack of cross-section images
+Usage: 
+    Put the script into the parent path of all image folders
+'''
+
+import heapq as pq
+import numpy as np
+import cv2 as cv
+import os
+from pathlib import Path
+
+PIXEL = 0.0061  # 1 pixel == 0.0061 mm
+
+
+def image_filter(img, h, th_g):
+    '''
+    Filter the image, apply a global threshold to get it binarized.
+    '''
+
+    equ = cv.equalizeHist(img)  # apply global hist equalization
+    gaus = cv.GaussianBlur(equ, (9, 9), 0)  # denoising using gaussian filter
+    # denoising using nonlocal means algorithm
+    dst1 = cv.fastNlMeansDenoising(gaus, None, h, 7, 21)
+
+    # apply Otsu's binarization (may be more stable)
+    # _, th1 = cv.threshold(dst1, th_g, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+    # apply global threshhold (may be more accurate)
+    _, th1 = cv.threshold(dst1, th_g, 255, cv.THRESH_BINARY)
+
+    return th1
+
+
+def cross_section_contour(img, little):
+    '''
+    Get the cross-section contour of the (original) image.
+    Output: 
+        The centroid coordinates and radius of the contour circle (reduced by a little).
+    '''
+
+    # create an intact cross-section
+    img_blur = cv.GaussianBlur(img, (15, 15), 0)
+    _, ostu2 = cv.threshold(img_blur, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+    # find the contour of the cross-section (largest one)
+    cnt = contour_sort_area(ostu2)
+    M = cv.moments(cnt[0])
+    cx = int(M['m10']/M['m00'])  # centroid
+    cy = int(M['m01']/M['m00'])
+    _, radius = cv.minEnclosingCircle(cnt[0])
+
+    return cx, cy, int(radius-little)
+
+
+def contour_sort_area(img):
+    '''
+    Sort the contours by the area, from max to min.
+    '''
+    contours, _ = cv.findContours(
+        img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+    # sort using key (contour area)
+    contours.sort(key=cv.contourArea, reverse=True)
+    return contours
+
+
+def cracks_extraction(img, c, num=1):
+    '''
+    Extract the cracks from the binary image (including some noises). 
+    Input: 
+        c = (cx, cy, r), contour circle of the cross-section
+        num, number of cracks to extract
+    Output: 
+        a list of cracks (max to min area)
+    '''
+    kernel = np.ones((3, 3), np.uint8)
+    # break thin bifurcations
+    img = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
+    # fill small holes
+    img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
+    cnts = contour_sort_area(img)
+
+    crack_poly = []
+    k = 0
+    for cnt in cnts:
+        # check if the contour is a pore
+        if(not is_pore(cnt, c)):
+            crack_poly.append(cnt)
+            k += 1
+        if(k >= num):
+            break
+
+    return crack_poly
+
+
+def is_pore(cnt, c):
+    '''
+    Determine of the contour is a internal pore
+    Input:
+        cnt, contour of the object
+        c = (cx, cy, r), contour circle of the cross-section
+    '''
+    side_length = np.sqrt(cv.contourArea(cnt))
+    M = cv.moments(cnt)
+    centx = int(M['m10']/M['m00'])  # centroid
+    centy = int(M['m01']/M['m00'])
+    depth = c[2] - np.sqrt((centx-c[0])*(centx-c[0])+(centy-c[1])*(centy-c[1]))
+
+    # internal pores criteria
+    if(depth > 2*side_length and side_length < 0.1*c[2]):
+        return True
+    else:
+        return False
+
+
+def calc_crack_geo(c, cnt):
+    '''
+    Calculate the geometric information of the contour (cnt)
+    Note: only valid if the crack doesn't contain center point and depth < r
+    Input: 
+        c = (cx, cy, r)
+        cnt, the contour of crack
+    Output: area, depth, side_length of the contour (pixel length)
+    '''
+    area = cv.contourArea(cnt)
+
+    # find the minimum distance between contour points and center using heap queue
+    dis_arr = []
+    for p in cnt:
+        dis = np.sqrt((p[0][0]-c[0])*(p[0][0]-c[0]) +
+                      (p[0][1]-c[1])*(p[0][1]-c[1]))
+        dis_arr.append(dis)
+    dis_smallest = pq.nsmallest(int(len(dis_arr)/4), dis_arr)
+    min_dis = np.mean(dis_smallest)
+
+    # get the convex hull of the crack
+    hull = cv.convexHull(cnt)
+    # draw the crack on a blank plate
+    # blank = np.zeros((736, 736), np.uint8)
+    # cv.drawContours(blank, [hull], 0, 250, -1)
+    # cv.imshow("Hull", blank)
+
+    # find the central angle of the crack hull
+    max_ang = 0
+    for p in hull:
+        vec1 = np.array([p[0][0]-c[0], p[0][1]-c[1]])
+        for q in hull:
+            vec2 = np.array([q[0][0]-c[0], q[0][1]-c[1]])
+            ang = calc_angle(vec1, vec2)
+            if(ang > max_ang):
+                max_ang = ang
+
+    return area, (c[2]-min_dis), c[2]*max_ang
+
+
+def calc_angle(vec1, vec2):
+    '''
+    Determine the angle between vectors vec1 and vec2
+    Input: vec1, vec2 are np.array
+    '''
+    cos = vec1.dot(vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2))
+    return np.arccos(np.clip(cos, -1.0, 1.0))
+
+
+def rotate2y(cnt, c):
+    '''
+    Rotate the contour with cernter c, make its centroid on the y-axis
+    Input:
+        cnt, (a numpy ndarray), the contour 
+        c, (cx, cy), the center coordinate
+    '''
+    M = cv.moments(cnt)
+    centx = int(M['m10']/M['m00'])  # centroid of the contour
+    centy = int(M['m01']/M['m00'])
+    v1 = np.array([centx-c[0], centy-c[1]])
+    v2 = np.array([0, 1])
+    r_ang = calc_angle(v1, v2)
+    if(centx-c[0] < 0):
+        r_ang = -r_ang
+
+    # rotate the contour
+    # print(r_ang*180/np.pi)
+    M = np.array([[np.cos(r_ang), -np.sin(r_ang)],
+                  [np.sin(r_ang), np.cos(r_ang)]])  # rotation matrix
+    # print(M)
+    cnt_rot = []
+    for p in cnt:
+        p_rot = M.dot(np.array([p[0][0]-c[0], p[0][1]-c[1]]))
+        cnt_rot.append([[int(p_rot[0]+c[0]), int(p_rot[1]+c[1])]])
+    return np.array(cnt_rot)
+
+
+def translate(cnt, c1, c2):
+    '''
+    Translate the contour from center c1 to center c2
+    '''
+    cnt_trans = []
+    for p in cnt:
+        cnt_trans.append(
+            [[int(p[0][0]+c2[0]-c1[0]), int(p[0][1]+c2[1]-c1[0])]])
+    return np.array(cnt_trans)
+
+
+def main():
+    # find the current directory
+    cwd = os.getcwd()
+    stepfolders = [dir for dir in os.listdir(cwd) if (
+        os.path.isdir(dir))]
+
+    # loop the loading step folders
+    for stepf in stepfolders:
+        if(stepf[-1] != 'C'):
+            continue
+
+        step_dir = os.path.join(cwd, stepf)
+
+        # create a new directory to store filtered images
+        filter_dir = os.path.join(cwd, stepf+"_filtered")
+        if(not os.path.isdir(filter_dir)):
+            os.mkdir(filter_dir)
+
+        # cross-section images of this step
+        imgs = os.listdir(step_dir)
+        # img_ref = cv.imread(os.path.join(
+        #    step_dir, imgs[0]), 0)  # to get image size
+
+        # loop the cross-section images in the current folder
+        for img in imgs:
+            img_array = cv.imread(os.path.join(
+                step_dir, img), 0)  # grayscale image
+            crack = image_filter(img_array, 12, 95)  # perform the filter
+
+            # store the contour circle size
+            cx, cy, r = cross_section_contour(img_array, 15)
+
+            # create a circle mask
+            cir_mask = np.zeros(img_array.shape[:2], np.uint8)
+            cv.circle(cir_mask, (cx, cy), r, 255, thickness=-1)
+
+            # apply bitwise operation to obtain the crack object
+            crack = cv.bitwise_not(crack, mask=cir_mask)
+
+            '''
+            # extract the most significant crack contour(s)
+            crack_num = 1
+            crack_hub = cracks_extraction(crack, (cx, cy, r), crack_num)
+
+            if(len(crack_hub) == 0):
+                crack_hub = np.array([])  # no crack detected
+
+            # draw the crack on a blank plate
+            blank = np.zeros(img_ref.shape[:2], np.uint8)
+            for i, c in enumerate(crack_hub):
+                cv.drawContours(blank, c, 0, 250, -1)
+            '''
+            # store the final image into the directory
+            cv.imwrite(os.path.join(filter_dir, Path(img).stem+".png"), crack)
+
+    # cv.waitKey()
+
+
+# run the program
+main()
+# cv.destroyAllWindows()
--- dadN_dK/fit.py
+++ dadN_dK/fit.py
@@ -1,28 +1,133 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import leastsq
 
-N = [80000, 90000, 100000, 105000, 115000, 125000,
-     130000, 135000, 140000, 150000, 160000]
-a = [22, 32, 37, 40, 42, 62, 66, 80, 98, 120, 166]
+PIXEL = 6.1e-6  # m
+dS = 200  # MPa
 
-N5 = np.array(N[0:5])
-a5 = np.array(a[0:5])
-
-
-def func(params, x):
-    '''Standard Form of Quadratic Function'''
-    a, b, c = params
-    return a * x * x + b * x + c
+# shape correction factor matrix
+M = [[[1.095, 0.113, -0.896], [-1.336, 1.824, 3.092], [13.108, -21.709, -4.197], [-43.689, 105.483, -13.255], [134.868, -271.225, 51.548], [-242.653, 387.47, -59.329], [254.093, -290.024, 13.481], [-108.196, 88.387, 10.854]],
+     [[-1.177, 0.271, 0.904], [17.924, -11.649, 0.701], [-137.252, 98.358, -32.641], [545.816, -415.027, 204.104], [-1223.334,
+                                                                                                                    982.713, -568.407], [1541.587, -1329.634, 857.543], [-1006.656, 961.893, -657.659], [264.206, -288.565, 191.57]],
+     [[0.725, -0.388, 0.008], [-17.427, 10.074, -4.883], [134.652, -80.088, 55.092], [-551.902, 328.165, -305.079], [1239.493, -772.921, 916.962], [-1548.537, 1055.952, -1545.428], [969.388, -784.581, 1372.595], [-227.132, 245.798, -485.556]]]
 
 
-def error(params, x, y):
-    '''Error function, that is, the difference between the value obtained by fitting curve and the actual value'''
-    return func(params, x) - y
+def calc_dK(a, h, D, x):
+    '''
+    Calculate the dK given the crack geometry
+    Input:
+        x, position of the interested point
+    '''
+    # compute b
+    b = 2*a*h/np.sqrt(-2*D*D+2*D*np.sqrt(D*D-4*h*h)+4*a*a+4*h*h)
+
+    a_b = a/b
+    a_D = a/D
+    x_h = x/h
+
+    # compute the shape correction factor
+    F = 0
+    for i in range(len(M)):
+        for j in range(len(M[0])):
+            for k in range(len(M[0][0])):
+                F += M[i][j][k]*(a_b**i)*(a_D**j)*(x_h**k)
+    # compute the stress intensity factor
+    dK = F*dS*np.sqrt(np.pi*a)
+    return dK
 
 
-def solvePara():
-    '''Solving parameters'''
-    p0 = [1, 1, 1]
-    para = leastsq(error, p0, args=(N5, a5))
-    return para
+def poly_fitting(N, a, hdouble):
+    '''
+    Fit the N-a (and N-h) curve using incremental polynominal method (2nd order), and
+    compute the dadN value using fitted curve.
+    Input: 
+        N, list of cycle number
+        a, crack length
+        hdouble, 2*h
+    '''
+    a_hat = []
+    h_hat = []
+    dadN = []
+    npt = len(N)-4  # number of fitted dadN point
+    if(npt < 0):
+        return a_hat, h_hat, [], dadN
+
+    for i in range(npt):
+        N5 = np.array(N[i:i+5])
+        C1 = 0.5*(N5[-1]+N5[0])
+        C2 = 0.5*(N5[-1]-N5[0])
+
+        n5 = (N5-C1)/C2  # normalized cycle number
+        a5 = np.array(a[i:i+5])*PIXEL
+        h5 = 0.5*np.array(hdouble[i:i+5])*PIXEL
+        (b2_a, b1_a, b0_a) = np.polyfit(n5, a5, 2)  # b0 + b1*n + b2*n^2
+        (b2_h, b1_h, b0_h) = np.polyfit(n5, h5, 2)  # b0 + b1*n + b2*n^2
+        # crack growth rate
+        dadN.append(b1_a/C2+2*b2_a*n5[2]/C2)
+        # fitted a and h
+        a_hat.append(b0_a + b1_a*n5[2] + b2_a*n5[2]*n5[2])
+        h_hat.append(b0_h + b1_h*n5[2] + b2_h*n5[2]*n5[2])
+    return a_hat, h_hat, N[2:-2], dadN
+
+
+def power_fitting(dK, dadN):
+    '''
+    Fit the dadN-dK curve using power law, i.e., dadN=a*dK^b
+    Output: 
+        a, b: fitting parameters
+    '''
+    log_dadN = np.log(dadN)
+    log_dK = np.log(dK)
+    (b, log_a) = np.polyfit(log_dK, log_dadN,  1)
+    return np.exp(log_a), b
+
+# test the curve fitting function
+# _, a_hat, h_hat, dadN = poly_fitting(N, a, hdouble)
+# a, b = power_fitting(N[2:-2], dadN)
+# plt.plot(N[2:-2], a*N[2:-2]**b)
+# plt.plot(N[2:-2], dadN)
+
+
+sample = "AMY6a"  # AMS2, AMG5, AMG6a, AMY6, AMY6a
+dadN_tot = []
+dK_tot = []
+N_tot = []
+a_tot = []
+h_tot = []
+with open(sample+".txt") as f:
+    lines = f.read().split('\n')
+    D = int(lines[0])
+    N, a, hdouble = [], [], []
+    for line in lines[1:]:
+        if(N == [] and line[0] == '#'):
+            continue
+        elif(N != [] and line[0] == '#'):
+            a_hat, h_hat, N_cut, dadN = poly_fitting(N, a, hdouble)
+            dK = []
+            for i in range(len(a_hat)):
+                dK.append(calc_dK(a_hat[i], h_hat[i], D, 0))
+            a_tot.append(a_hat)
+            h_tot.append(h_hat)
+            N_tot.append(N_cut)
+            dadN_tot.append(dadN)
+            dK_tot.append(dK)
+            N, a, hdouble = [], [], []
+        else:
+            data = line.split()
+            N.append(int(data[0]))
+            a.append(int(data[1]))
+            hdouble.append(int(data[2]))
+
+# print(len(dadN_tot), len(dK_tot), len(N_tot), len(a_tot), len(h_tot))
+# output data into txt file
+# a, h, N, dadN, dK
+out = open(sample+"_data.txt", "a")
+for i in range(len(N_tot)):
+    out.write('#'+str(i+1)+'\n')
+    out.write("{:>16} {:>16} {:>10} {:>10} {:>10} \n".format(
+        "a", "h", "N", "dadN", "dK"))
+    for k in range(len(N_tot[i])):
+        out.write("{:>16.6} {:>16.6} {:>10} {:>10.6} {:>10.6} \n".format(a_tot[i][k], h_tot[i][k], N_tot[i]
+                  [k], dadN_tot[i][k], dK_tot[i][k]))
+out.close()
+
+plt.show()
